fead450162f72af06b0fd07856033b50584d26b9
Daniel Galleni <danielgalleni@gmail.com>
2017-03-25 15:36:45 -0300

    Inclusão de teste e alteração do condicional removendo o else

--- app.rb
+++ app.rb
@@ -5,16 +5,15 @@ require 'httparty'
 
 get '/' do
 	endereco = params["url"]
+	parsed = nil
+
 	if (endereco != nil)
 		escaped = URI.escape(endereco)
 		url = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
 		response = HTTParty.get(url)
 		parsed   = JSON.parse(response.body)['results']	
-		erb :index, :locals => {result: parsed}
-	else
-		erb :index, :locals => {result: nil}
 	end
-
-
 	
+	erb :index, :locals => {result: parsed}
+
 end
